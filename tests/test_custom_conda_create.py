e9606f58118840e9b1df3d8a82d90789bc5b56cd
bk-m <kolja.mielsch@googlemail.com>
2018-05-21 12:48:41 +0200

    removed sample tests

--- tests/test_custom_conda_create.py
+++ tests/test_custom_conda_create.py
@@ -10,6 +10,7 @@ from click.testing import CliRunner
 from custom_conda_create import custom_conda_create
 from custom_conda_create import cli
 from custom_conda_create.util import shell_funcs
+from custom_conda_create.util import click_funcs
 
 
 class TestCustom_conda_create(unittest.TestCase):
@@ -26,10 +27,10 @@ class TestCustom_conda_create(unittest.TestCase):
 
     def test_command_line_interface(self):
         """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'custom_conda_create.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        # runner = CliRunner()
+        # result = runner.invoke(cli.main)
+        # assert result.exit_code == 0
+        # assert 'custom_conda_create.cli.main' in result.output
+        # help_result = runner.invoke(cli.main, ['--help'])
+        # assert help_result.exit_code == 0
+        # assert '--help  Show this message and exit.' in help_result.output
